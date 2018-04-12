08215269fcfc207ae46ed4a6c23b7275540591cd
JoeySolak <8jsolak@stpatrick.org>
2018-04-12 09:22:30 -0500

    Add files via upload

--- listTasks.js
+++ listTasks.js
@@ -1,6 +1,6 @@
 //Task 1
 function largest(list){
-  let largest=lists[0]
+  let largest=list[0]
   for(let l=0; l < list.length; l++){
     if (list[l]>largest){
       //update
@@ -12,25 +12,36 @@ function largest(list){
 
 //Task 2
 function reverse(list){
-let reverselist = []
-for(c=8; c > reverse.list; c)
+let reverselist = [];
+for(c=8; c > reverse.list; c++){
+
+  }
 }
 
 //Task 3
 function cotains(list,e){
-
+let containslist = [];
+  for(a=0; a < list.length; a++){
+    if(list[a]== e){
+      return "true"
+    }
+  }
+  return "false"
 }
 
 //Task 4
 function oddElements(list){
-let oddlist = []
+let oddlist = [];
 for(let b=1; b < b.length; a = a + 2);
 oddlist.push(list.length)
 }
 
 //Task 5
 function total(list){
-
+  let sum =0;
+  for(let a=0; a < list.length; a++)
+  sum = sum + list[a]
+  return sum;
 }
 
 //Task 6
@@ -68,7 +79,7 @@ function rotate(list,k){
 function fibonacci(){
 
 }
-
-function main(){
-  lists.largest = [0,1,2,3,4,5,6,7,8];
-}
+console.log(cotains([0,1,2,3,4,5,6,7,8],3));
+console.log(total([0,1,2,3,4,5,6,7,8]));
+console.log(largest([0,1,2,3,4,5,6,7,8]));
+console.log(odd([0,1,2,3,4,5,6,7,8]));
